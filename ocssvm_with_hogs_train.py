81bfabb41121f2df0132a468c1f9f8029a1b75a6
Hisaan-Anjum <the-cuboid@hotmail.com>
2024-01-10 06:57:18 +0500

    first commit

---
+++ ocssvm_with_hogs_train.py
@@ -0,0 +1,76 @@
+import os
+import numpy as np
+import cv2
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from skimage.feature import hog
+from keras.preprocessing.image import ImageDataGenerator
+import joblib
+
+def augment_images(directory, augmentation_factor):
+    datagen = ImageDataGenerator(
+        rotation_range=10,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        shear_range=0.1,
+        zoom_range=0.1,
+        horizontal_flip=True,
+        fill_mode='nearest'
+    )
+
+    image_files = [os.path.join(directory, file) for file in os.listdir(directory) if file.endswith(('.jpg', '.png'))]
+
+    augmented_images = []
+
+    for image_file in image_files:
+        img = cv2.imread(image_file, cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (256, 256))
+        img = img.astype('float32') / 255.0
+        img = np.expand_dims(img, axis=-1)
+        img = np.expand_dims(img, axis=0)
+
+        for _ in range(augmentation_factor):
+            augmented_img = datagen.flow(img).next()[0]
+            augmented_images.append(augmented_img)
+
+    return np.array(augmented_images)
+
+def extract_hog_features(images):
+    features = []
+    for img in images:
+        # Also please try 8,8 pixels per cell if there is enough computing power
+        _, hog_feature = hog(img.reshape((256, 256)), orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), block_norm='L2-Hys', visualize=True)
+        features.append(hog_feature.flatten())
+    return np.array(features)
+
+def train_one_class_svc(X_train):
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+
+    y_train = np.ones(X_train_scaled.shape[0])
+
+    svm_classifier = SVC(kernel='linear', class_weight='balanced')
+    svm_classifier.fit(X_train_scaled, y_train)
+
+    return svm_classifier
+
+def save_classifier(classifier, filename='ct_ocsvm.joblib'):
+    joblib.dump(classifier, filename)
+    print(f'Trained classifier saved to {filename}')
+
+if __name__ == "__main__":
+    normal_images_directory = 'DATASET/TRAIN/NORMAL'
+    augmentation_factor = 5
+
+    normal_augmented = augment_images(normal_images_directory, augmentation_factor)
+
+    X_train, _, _, _ = train_test_split(normal_augmented, np.zeros(normal_augmented.shape[0]), test_size=0.2, random_state=42)
+
+    X_train_hog = extract_hog_features(X_train)
+
+    one_class_svc_classifier = train_one_class_svc(X_train_hog)
+
+    save_classifier(one_class_svc_classifier, filename='ct_ocsvm.joblib')
+
+    print('Trained & saved!')
---
+++ ocsvm_with_hogs_test.py
@@ -0,0 +1,42 @@
+import os
+import cv2
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from skimage.feature import hog
+from keras.models import load_model
+
+def extract_hog_features(images):
+    features = []
+    for img in images:
+        _, hog_feature = hog(img.reshape((256, 256)), orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), block_norm='L2-Hys', visualize=True)
+        features.append(hog_feature.flatten())
+    return np.array(features)
+
+def load_and_test_model(model_filename, test_images_directory):
+    loaded_model = load_model(model_filename)
+    test_images = []
+    for file in os.listdir(test_images_directory):
+        if file.endswith(('.jpg', '.png')):
+            img = cv2.imread(os.path.join(test_images_directory, file), cv2.IMREAD_GRAYSCALE)
+            img = cv2.resize(img, (256, 256))
+            img = img.astype('float32') / 255.0
+            img = np.expand_dims(img, axis=-1)
+            img = np.expand_dims(img, axis=0)
+
+            test_images.append(img)
+
+    test_images = np.array(test_images)
+    test_hog_features = extract_hog_features(test_images)
+
+    scaler = StandardScaler()
+    test_hog_features_scaled = scaler.fit_transform(test_hog_features)
+
+    predictions = loaded_model.predict(test_hog_features_scaled)
+
+    for i, prediction in enumerate(predictions):
+        print(f"Image {i + 1} - Predicted Class: {'Normal' if prediction > 0.5 else 'Abnormal'}")
+
+model_filename = 'ct_ocsvm.joblib'
+#normal test = test_images_directory = 'DATASET/TEST/NORMAL'
+test_images_directory = 'DATASET/TEST/ABNORMAL/HEMORRHAGE'
+load_and_test_model(model_filename, test_images_directory)
